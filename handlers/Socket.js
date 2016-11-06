b8d1d96b1fd9700f770e356515ff9a5d1c010fbd
riyasopr <riyasbitz@gmail.com>
2016-11-06 16:28:33 +0530

    Update Socket.js

--- handlers/Socket.js
+++ handlers/Socket.js
@@ -1,6 +1,6 @@
 
 var Socket = function (app, io) {
-
+/*
         io.on('connection', function (client) {
             var remote = client.handshake;
             console.log('A client has been connected IP: ' + remote.address + " On: " + remote.time + " Platform: " + remote.headers['user-agent']);
@@ -13,6 +13,6 @@ var Socket = function (app, io) {
                 console.log('1 Client has been disconnected, ' + clientLists.length + " still connected");
             });
         });
-
+*/
 }
 module.exports = Socket;
